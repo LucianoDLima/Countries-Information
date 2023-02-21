c6ab05aac9cbec64f87634f8f7857b81e1c32f90
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-02-21 07:12:45 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+async function teste(country) {
+    // const response = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+    // const data = await response.json()
+    console.log(data);
+    // return response
+}
+
+teste('brazil')
